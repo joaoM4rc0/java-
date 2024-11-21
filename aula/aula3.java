88854eab59082d66c0b685302a5d074ad3f76464
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-21 16:01:09 -0300

    atualizando

---
+++ aula/aula3.java
@@ -0,0 +1,11 @@
+// package aprendendo.aula
+public class aula3 {
+    public static void main(String[] args) {
+        int a = 5 + 8;
+        int b = 5 - 8;
+        int c = 5 * 8;
+        int d = 5 / 8;
+        int e = 15 % 8;
+        System.out.println(e <= c);
+    }
+}

f1ed63de002af432af95be68e4307418b0349c81
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-21 16:18:47 -0300

    removendo pastas

--- aprendendo/aula/aula1.java
+++
@@ -1,8 +0,0 @@
-package aprendendo.aula;
-
-
-public class aula1 {
-    public static void main(String[] args) {
-        System.out.println("KA-ME-HA-MA-HAAAAAAAAAAAAAAAAAA");
-    }
-}
--- aprendendo/aula/aula2.java
+++
@@ -1,9 +0,0 @@
-package aprendendo.aula;
-/**
- * esse é um comentario
- */
-public class aula2 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}

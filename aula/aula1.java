871cfc8ab0c375809d63a675ef277559a7569203
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-22 19:43:45 -0300

    removendo pasta

--- aula/aula1.java
+++
@@ -1,6 +0,0 @@
-
-public class aula1 {
-    public static void main(String[] args) {
-        System.out.println("KA-ME-HA-MA-HAAAAAAAAAAAAAAAAAA");
-    }
-}
--- aula/aula2.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * esse é um comentario
- */
-public class aula2 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}

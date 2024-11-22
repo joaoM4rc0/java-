7ce8b3d99d186c97858c6914b85ff57501173f4a
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-22 20:50:30 -0300

    estudando orientação a objetos

---
+++ java_OOP/dominio/Estudante.java
@@ -0,0 +1,26 @@
+package java_OOP.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char Sexo) {
+        this.sexo = sexo;
+    }
+}

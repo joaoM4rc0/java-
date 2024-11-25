455315f02b5e51e8c45a0324f838d3ecca677a36
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-25 20:33:46 -0300

    .

---
+++ exercicios/test/FuncionariosTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import dominio.Funcionarios;
+
+public class FuncionariosTest {
+    public static void main(String[] args) {
+        Funcionarios funcionarios = new Funcionarios();
+
+        funcionarios.nome = "joao";
+        funcionarios.idade = 18;
+        funcionarios.salario = new double[]{1000, 2000, 3000};
+        funcionarios.ImprimeDados();
+        System.out.println("------------------");
+        funcionarios.mediaSalarial(funcionarios.salario);
+    }
+}

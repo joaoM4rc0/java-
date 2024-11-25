9cb21f0292c9803a875f631f1ed944dbb3409eb9
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-25 20:33:23 -0300

    .

---
+++ exercicios/dominio/Funcionarios.java
@@ -0,0 +1,31 @@
+package dominio;
+
+public class Funcionarios {
+    public String nome; 
+    public int idade; 
+    public double[] salario;
+    private double salarioSomado;
+    public void ImprimeDados() {
+        if (salario == null || salario.length == 0) {
+            System.out.println("Erro: Salário não disponível."); 
+            return;
+        } 
+        salarioSomado = 0;
+        for (double d : salario) {
+            salarioSomado += d;           
+        }
+        System.out.println("nome: " + this.nome + ", idade: " + this.idade + " e esse é o meu salario: " + this.salarioSomado +"R$");
+    }
+    public void mediaSalarial(double... salario) {
+        if (salario == null || salario.length == 0) {
+            System.out.println("Erro: Nenhum salário fornecido.");
+            return; 
+        }
+        double somaSalario = 0;
+        for (double d : salario) {
+            somaSalario += d;
+        }
+        double mediaDoSalario = somaSalario / salario.length;
+        System.out.println("media salarial: " + mediaDoSalario + "R$");
+    }
+}

07d45b6ba8c15232cc3bc8e030b0a3e2790c4d89
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-25 18:49:46 -0300

    .

--- java_OOP/dominio02/Estudante.java
+++ java_OOP/dominio02/Estudante.java
@@ -4,4 +4,7 @@ public class Estudante {
     public String nome;
     public int idade;
     public char sexo;
+    public void Imprime() { 
+        System.out.println("nome: " + this.nome + " idade: " + this.idade + " sexo: " + this.sexo);
+    }
 }
--- java_OOP/dominio02/ImprimeEstudante.java
+++
@@ -1,7 +0,0 @@
-package java_OOP.dominio02;
-
-public class ImprimeEstudante {
-    public void Imprime(Estudante estudante) { 
-        System.out.println("nome: " + estudante.nome + " idade: " + estudante.idade + " sexo: " + estudante.sexo);
-    }
-}

4f4b7ecebca85186c34cbc55ca92202b07897b95
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-29 20:40:17 -0300

    .

---
+++ java_OOP/associacao/dominio/Time.java
@@ -0,0 +1,15 @@
+package java_OOP.associacao.dominio;
+
+public class Time {
+    private String nome;
+
+    public Time(String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

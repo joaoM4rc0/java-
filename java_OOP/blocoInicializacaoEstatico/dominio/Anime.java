6402a770c980d4d4106f50c73ff230a44e3c7ac7
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-28 21:26:41 -0300

    .

---
+++ java_OOP/blocoInicializacaoEstatico/dominio/Anime.java
@@ -0,0 +1,31 @@
+package java_OOP.blocoInicializacaoEstatico.dominio;
+
+public class Anime {
+        private String nome;
+        private static int[] eps;
+        static {
+            System.err.println("no bloco estático");
+            eps = new int[100]; 
+            for (int i = 0; i < eps.length; i++) {
+                eps[i] = i + 1;
+            }
+        }
+         {
+            System.err.println("no bloco não estático");
+        }
+        public Anime(String nome) {
+            this.nome = nome;
+        }
+        public Anime() {
+            for (int i : eps) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        public String getNome() {
+            return nome;
+        }
+        public int[] getEps() {
+            return eps;
+        }
+    }

9d15c590bfa8f3f301b5f71a8861827bb360474f
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-29 21:55:36 -0300

    .

---
+++ java_OOP/associacao/test02/JogadoresTest.java
@@ -0,0 +1,32 @@
+package java_OOP.associacao.test02;
+
+import java_OOP.associacao.dominio02.Clube;
+import java_OOP.associacao.dominio02.Jogador;
+
+public class JogadoresTest {
+    public static void main(String[] args) {
+        Clube time = new Clube("corinthians");
+
+        Jogador jogador1 = new Jogador("depay");
+        Jogador jogador2 = new Jogador("romero");
+        Jogador jogador3 = new Jogador("garro");
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        jogador1.setTime(time);
+        jogador2.setTime(time);
+        jogador3.setTime(time);
+
+        time.setJogadores(jogadores);
+
+        System.out.println("----Time----");
+        time.imprime();
+        System.out.println();
+        System.out.println("----jogador----");
+        jogador1.imprime();
+        System.out.println("----jogador----");
+        jogador2.imprime();
+        System.out.println("----jogador----");
+        jogador3.imprime();
+        System.out.println("-------------");
+    }
+}

be0de104ff7859cfe8ad2ad9b1e29cad734d434a
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-29 21:11:09 -0300

    .

---
+++ java_OOP/associacao/dominio/Escola.java
@@ -0,0 +1,30 @@
+package java_OOP.associacao.dominio;
+
+public class Escola {
+    private String nome;
+    private Professor[] professores;
+    
+    public Escola(String nome) {
+        this.nome = nome;
+    }
+    public Escola(String nome, Professor[] professor) {
+        this.nome = nome;
+        this.professores = professor;
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+
+        if (professores == null) return;
+
+        for (Professor professor : professores) {
+            System.out.println(professor.getNome());
+        }
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

d562ca64b2e3d367b6da6d81c440a41dd73b539a
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-12-02 20:34:57 -0300

    "."

---
+++ java_POO/iSobreescrita/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package java_POO.iSobreescrita.test;
+
+import java_POO.iSobreescrita.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("DR. stone");
+        System.out.println(anime);
+    }
+}

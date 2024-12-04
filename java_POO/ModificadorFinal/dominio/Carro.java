ff42460c03157972a57ebec879b8395ba3058424
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-12-04 20:10:28 -0300

    .

--- java_POO/ModificadorFinal/dominio/Carro.java
+++ java_POO/ModificadorFinal/dominio/Carro.java
@@ -2,5 +2,5 @@ package java_POO.ModificadorFinal.dominio;
 
 public class Carro {
     public static final double VELOCIDADE_LIMITE = 250;
-
+    private final Comprador comprador = new Comprador();
 }
--- java_POO/ModificadorFinal/test/CarroTest01.java
+++ java_POO/ModificadorFinal/test/CarroTest01.java
@@ -6,5 +6,6 @@ public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+
     }
 }

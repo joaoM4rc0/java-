24910e9dccb17f667da2ecc3a764cdc3c0f9448b
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-12-09 18:20:59 -0300

    tratamento de exceções

---
+++ java_POO/OException/test/SatckOverFlowTest.java
@@ -0,0 +1,10 @@
+package java_POO.OException.test;
+
+public class SatckOverFlowTest {
+    public static void main(String[] args) {
+        recursiva();
+    }
+    public static void recursiva() {
+//        recursiva();
+    }
+}

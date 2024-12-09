2f0887481ee8106107852dee64b52237e0774475
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-12-09 18:42:45 -0300

    tratamento de exceções

--- java_POO/OException/runTime/test/runTimeExceptionTest.java
+++ java_POO/OException/runTime/test/runTimeExceptionTest.java
@@ -1,4 +1,8 @@
 package java_POO.OException.runTime.test;
 
 public class runTimeExceptionTest {
+    public static int divisaoPorZero = 5 / 0;
+    public static void main(String[] args) {
+        System.out.println(divisaoPorZero);
+    }
 }

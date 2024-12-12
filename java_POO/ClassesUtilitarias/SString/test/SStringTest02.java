a53aaa1c63d0ab19e2c9cb6e96d410b5f7eb0ff4
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-12-12 20:09:01 -0300

    .

---
+++ java_POO/ClassesUtilitarias/SString/test/SStringTest02.java
@@ -0,0 +1,13 @@
+package java_POO.ClassesUtilitarias.SString.test;
+
+public class SStringTest02 {
+    public static void main(String[] args) {
+        String nome = "guts";
+        for (int i = 0 ; i < nome.length(); i++ ) {
+            System.out.println(nome.charAt(i));
+        }
+        System.out.println(nome.replace("g", "l"));
+
+        System.out.println(nome.substring(0, nome.length()));
+    }
+}

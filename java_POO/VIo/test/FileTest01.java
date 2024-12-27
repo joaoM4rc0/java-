c36579dee0a3296fe6515fcfac268f2cb190d1ba
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-12-27 20:43:54 -0300

    .

--- java_POO/VIo/test/FileTest01.java
+++ java_POO/VIo/test/FileTest01.java
@@ -1,4 +1,20 @@
 package java_POO.VIo.test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FileTest01 {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\joaom\\OneDrive\\Documents\\aprendendo_java\\aprendendo\\arquivo\\file.txt");
+        try {
+            boolean newFile = file.createNewFile();
+            System.out.println(newFile);
+            if (file.exists()){
+                boolean delete = file.delete();
+                System.out.println(delete);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

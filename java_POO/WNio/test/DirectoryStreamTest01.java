0685c0aa410dd30a569b17f45ca601ebd3279f76
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-05 16:57:57 -0300

    .

---
+++ java_POO/WNio/test/DirectoryStreamTest01.java
@@ -0,0 +1,18 @@
+package java_POO.WNio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest01 {
+    public static void main(String[] args) throws IOException{
+        Path dir = Paths.get(".");
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path path: stream) {
+                System.out.println(path.getFileName());
+            }
+        }
+    }
+}

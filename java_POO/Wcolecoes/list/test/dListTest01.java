6ad2da0ca32ce9975ad2680d3b13ef0471371813
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-09 21:20:06 -0300

    .

--- java_POO/Wcolecoes/list/test/dListTest01.java
+++ java_POO/Wcolecoes/list/test/dListTest01.java
@@ -6,13 +6,15 @@ import java.util.List;
 public class dListTest01 {
     public static void main(String[] args) {
         List<String>nomes = new ArrayList<>();
+        List<String>nomes2 = new ArrayList<>();
         nomes.add("joao");
         nomes.add("maria");
+        nomes2.add("jonas");
+        nomes2.add("marcio");
+        nomes.addAll(nomes2);
         for (String nome : nomes) {
             System.out.println(nome);
         }
-        for(int i = 0 ; i < nomes.size(); i++) {
-            System.out.println(nomes.get(i));
-        }
+
     }
 }

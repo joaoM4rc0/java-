bf3e6adcf313b92e2c6397822593f1786cafb47c
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-11 17:27:30 -0300

    .

--- java_POO/Wcolecoes/list/dominio/Manga.java
+++ java_POO/Wcolecoes/list/dominio/Manga.java
@@ -65,7 +65,8 @@ public class Manga implements Comparable<Manga>{
 
     @Override
     public int compareTo(@NotNull  Manga outroManga) {
-        return Double.compare(this.preco, outroManga.getPreco());
+        return this.nome.compareTo(outroManga.getNome());
+//        return Double.compare(this.preco, outroManga.getPreco());
 //        return Double.valueOf(this.preco).compareTo(outroManga.getPreco());
 //        return Long.compare(this.id, outroManga.getId());
     }

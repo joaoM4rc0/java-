9f37538d4bd953b3fc8104977d3d4618aa079962
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-14 16:35:54 -0300

    .

---
+++ java_POO/generics/test/GenericsTest01.java
@@ -0,0 +1,25 @@
+package java_POO.generics.test;
+
+import java_POO.Wcolecoes.list.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        nomes.add("joao");
+        nomes.add("joao");
+        for (String nome: nomes) {
+            System.out.println(nome);
+        }
+        add(nomes, new Consumidor("marco"));
+        for (String nome: nomes) {
+            System.out.println(nome);
+            // da erro: ClassCastException
+        }
+    }
+    private static <T> void add(List<T> nomes, T consumidor) {
+        nomes.add(consumidor);
+    }
+}
---
+++ java_POO/generics/test/WildcardTest01.java
@@ -0,0 +1,7 @@
+package java_POO.generics.test;
+
+public class WildcardTest01 {
+    public static void main(String[] args) {
+
+    }
+}

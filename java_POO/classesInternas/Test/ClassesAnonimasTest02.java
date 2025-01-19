131b96283f4f4a159621e22a28a80e787587ac71
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-18 21:14:00 -0300

    classes Anonimas

--- java_POO/classesInternas/Test/ClassesAnonimasTest02.java
+++ java_POO/classesInternas/Test/ClassesAnonimasTest02.java
@@ -2,11 +2,21 @@ package java_POO.classesInternas.Test;
 
 import java_POO.generics.dominio.Barco;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ClassesAnonimasTest02 {
     public static void main(String[] args) {
-        List<Barco> barcos = List.of(new Barco("barquinho"), new Barco("outro barquinho"));
-
+        List<Barco> barcos = new ArrayList<>();
+        barcos.add(new Barco("lancha"));
+        barcos.add(new Barco("canoa"));
+        barcos.sort(new Comparator<Barco>() {
+            @Override
+            public int compare(Barco o1, Barco o2) {
+                return o1.getNome().compareTo(o2.getNome());
+            }
+        });
+        System.out.println(barcos);
     }
 }

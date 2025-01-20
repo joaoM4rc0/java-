e55a0840797a4960b41789bd7f7d650cc62d50e3
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-20 14:58:19 -0300

    classes estaticas

--- java_POO/classesInternas/Test/ClassesEstaticasTest01.java
+++ java_POO/classesInternas/Test/ClassesEstaticasTest01.java
@@ -1,17 +1,27 @@
 package java_POO.classesInternas.Test;
 
-class Externa {
-    private static String mensagem = "olá estou no metodo interno";
-    public static class AnhinhadaEstatica {
-        public void exibir() {
-            System.out.println(mensagem);
-        }
-    }
-}
 public class ClassesEstaticasTest01 {
-    public static void main(String[] args) {
-        Externa.AnhinhadaEstatica anhinhadaEstatica = new Externa.AnhinhadaEstatica();
-        anhinhadaEstatica.exibir();
+    private String numeroPedido;
+
+    public ClassesEstaticasTest01(String numeroPedido) {
+        this.numeroPedido = numeroPedido;
     }
 
+    // Classe aninhada estática para representar o status dos pedidos
+    public static class Status {
+        public static final String PENDENTE = "Pendente";
+        public static final String PROCESSADO = "Processado";
+        public static final String ENTREGUE = "Entregue";
+    }
+
+    public void imprimirStatus(String status) {
+        System.out.println("O pedido " + numeroPedido + " está " + status);
+    }
 }
+class Main {
+    public static void main(String[] args) {
+        ClassesEstaticasTest01 pedido = new ClassesEstaticasTest01("12345");
+        pedido.imprimirStatus(ClassesEstaticasTest01.Status.PENDENTE); // Saída: O pedido 12345 está Pendente
+    }
+}
+

a7075f0a22188252ba42743cec986e3f965e7c89
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-22 15:28:18 -0300

    .

--- java_POO/ZCLambdas/test/MethodReferenceTest03.java
+++ java_POO/ZCLambdas/test/MethodReferenceTest03.java
@@ -6,11 +6,13 @@ import java_POO.ZCLambdas.service.AnimeComparators;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public class MethodReferenceTest03 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of("davi", "mathias", "ronaldo"));
         list.sort(String::compareTo);
-        System.out.println(list);
+        BiPredicate<List<String>, String> checkdName = List::contains;
+        System.out.println(checkdName.test(list, "davi"));
     }
 }

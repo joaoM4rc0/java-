539b9b37cb1a08f5d27b2e0074b8e740dd897ec0
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-22 15:13:37 -0300

    .

--- java_POO/ZCLambdas/service/AnimeComparators.java
+++ java_POO/ZCLambdas/service/AnimeComparators.java
@@ -2,11 +2,14 @@ package java_POO.ZCLambdas.service;
 
 import java_POO.ZCLambdas.dominio.Anime;
 
-public class AnimeComparators {
+public class  AnimeComparators {
     public static int compareByTitle(Anime a1, Anime a2) {
         return a1.getName().compareTo(a2.getName());
     }
     public static int compareByEps(Anime a1, Anime a2) {
+        return Integer.compare(a1.getEps(), a2.getEps());
+    }
+    public int compareByEpsNoStatic(Anime a1, Anime a2) {
         return Integer.compare(a1.getEps(), a2.getEps());
     }
 }
---
+++ java_POO/ZCLambdas/test/MethodReferenceTest02.java
@@ -0,0 +1,17 @@
+package java_POO.ZCLambdas.test;
+
+import java_POO.ZCLambdas.dominio.Anime;
+import java_POO.ZCLambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("berserk", 45), new Anime("one piece", 1050), new Anime("black clover", 170)));
+
+        animeList.sort(animeComparators::compareByEpsNoStatic);
+        System.out.println(animeList);
+    }
+}

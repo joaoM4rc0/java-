9ddcb39ba9be61daf3fedc11fb6fe62b9749ceeb
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-23 16:42:53 -0300

    .

--- java_POO/ZDStream/test/StreamTest03.java
+++ java_POO/ZDStream/test/StreamTest03.java
@@ -5,16 +5,16 @@ import java_POO.ZDStream.dominio.LightNovel;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 // ordernar por title
 // retornar 3 light novels com valor menor do que 4
 // resolvido
-public class StreamTest02 {
+public class StreamTest03 {
     private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
                     new LightNovel("youkoso jitsuryoko", 2.5),
                     new LightNovel("one punch man ", 2.7),
+                    new LightNovel("one piece", 3.7),
                     new LightNovel("one piece", 3.7),
                     new LightNovel("dandandan", 5.8),
                     new LightNovel("overlord", 7.7),
@@ -22,12 +22,10 @@ public class StreamTest02 {
     )
     );
     public static void main(String[] args) {
-        List<String> title = lightNovels.stream()
-                .sorted(Comparator.comparing(LightNovel::getTitle))
-                .filter(l -> l.getPreco() <= 4)
-                .limit(3)
-                .map(LightNovel::getTitle)
-                .collect(Collectors.toList());
-        System.out.println(title);
+        lightNovels.forEach(System.out::println);
+        long count = lightNovels.stream().filter(ln -> ln.getPreco() <= 4).count();
+        System.out.println(count);
+        long count1 = lightNovels.stream().distinct().filter(ln -> ln.getPreco() <= 4).count();
+        System.out.println(count1);
     }
 }

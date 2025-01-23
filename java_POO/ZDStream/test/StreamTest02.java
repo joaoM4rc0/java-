aad58eecbb72147b4f1854b66931048c0b463506
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-23 16:06:48 -0300

    vou melhorar esse codigo utilizando Stream

---
+++ java_POO/ZDStream/test/StreamTest02.java
@@ -0,0 +1,32 @@
+package java_POO.ZDStream.test;
+
+import java_POO.ZDStream.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+// ordernar por title
+// retornar 3 light novels com valor menor do que 4
+public class StreamTest02 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+                    new LightNovel("youkoso jitsuryoko", 2.5),
+                    new LightNovel("one punch man ", 2.7),
+                    new LightNovel("one piece", 3.7),
+                    new LightNovel("dandandan", 5.8),
+                    new LightNovel("overlord", 7.7),
+                    new LightNovel("monogatari", 3.9)
+    )
+    );
+    public static void main(String[] args) {
+        List<String> title = lightNovels.stream()
+                .sorted(Comparator.comparing(LightNovel::getTitle))
+                .filter(l -> l.getPreco() <= 4)
+                .limit(3)
+                .map(LightNovel::getTitle)
+                .collect(Collectors.toList());
+        System.out.println(title);
+    }
+}

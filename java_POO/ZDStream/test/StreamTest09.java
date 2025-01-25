5fe820f9137990460cd760d64469734525cea7bb
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-25 19:50:04 -0300

    .

---
+++ java_POO/ZDStream/test/StreamTest09.java
@@ -0,0 +1,27 @@
+package java_POO.ZDStream.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+
+public class StreamTest09 {
+    public static void main(String[] args) {
+        Stream.iterate(1,n -> n + n)
+                .limit(20)
+                .forEach(System.out::println);
+        Stream.iterate(new int[]{0,1}, n -> new int[]{n[1], n[0] + n[1]})
+                .limit(10)
+                .map(n -> n[0])
+                .forEach(System.out::println);
+    }
+}
+
+/*
+retorna:
+Neymar é o melhor jogador do futebol brasileiro
+o melhor anime é hajime no ippo
+melhor
+ */

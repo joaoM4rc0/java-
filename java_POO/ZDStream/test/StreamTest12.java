ab1ec4282b1472e02c5151615900778bcf9d1d05
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-27 14:48:27 -0300

    .

--- java_POO/ZDStream/test/StreamTest12.java
+++ java_POO/ZDStream/test/StreamTest12.java
@@ -4,13 +4,12 @@ import java_POO.ZDStream.dominio.Category;
 import java_POO.ZDStream.dominio.LightNovel;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 
-public class StreamTest13 {
+public class StreamTest12 {
     private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
             new LightNovel("youkoso jitsuryoko", 2.5, Category.DRAMA),
             new LightNovel("one punch man ", 2.7, Category.ACAO),
@@ -21,6 +20,26 @@ public class StreamTest13 {
     )
     );
     public static void main(String[] args) {
+        //        Map<Category, List<LightNovel>> categoryLightNovel = new HashMap<>();
+//            List<LightNovel> drama =new ArrayList<>();
+//            List<LightNovel> acao =new ArrayList<>();
+//            List<LightNovel> romance =new ArrayList<>();
+//            List<LightNovel> fantasy =  new ArrayList<>();
+//        for (LightNovel lightNovel : lightNovels) {
+//            switch (lightNovel.getCategory()) {
+//                case ACAO: acao.add(lightNovel); break;
+//                case DRAMA: drama.add(lightNovel); break;
+//                case FANTASY: fantasy.add(lightNovel); break;
+//                case ROMANCE: romance.add(lightNovel); break;
+//            }
+//        }
+//        categoryLightNovel.put(Category.ACAO, acao);
+//        categoryLightNovel.put(Category.DRAMA, drama);
+//        categoryLightNovel.put(Category.ROMANCE, romance);
+//        categoryLightNovel.put(Category.FANTASY, fantasy);
+//        System.out.println(categoryLightNovel);
+
+        // agora iremos utilizar o stream com o grouping by para melhorar esse codigo
         Map<Category, List<LightNovel>> collect = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory));
         System.out.println(collect);
     }

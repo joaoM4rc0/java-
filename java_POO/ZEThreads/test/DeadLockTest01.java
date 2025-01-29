e8463a9778b93fb8f6d04436f1753ffc6c4611b7
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-01-28 21:36:24 -0300

    classes thread safes

---
+++ java_POO/ZEThreads/test/DeadLockTest01.java
@@ -0,0 +1,30 @@
+package java_POO.ZEThreads.test;
+
+public class DeadLockTest01 {
+    public static void main(String[] args) {
+        Object lock1  =new Object();
+        Object lock2  =new Object();
+
+        Runnable r1 = () -> {
+            synchronized (lock1) {
+                System.out.println("Thread 1: Segurando lock 1 ");
+                System.out.println("Thread 1: esperando lock 2 ");
+                synchronized (lock2) {
+                    System.out.println("Thread 1: Segurando lock 2 ");
+                }
+            }
+        };
+        Runnable r2 = () -> {
+            synchronized (lock2) {
+                System.out.println("Thread 2: Segurando lock 1 ");
+                System.out.println("Thread 2: esperando lock 2 ");
+                synchronized (lock1) {
+                    System.out.println("Thread 2: Segurando lock 1 ");
+                }
+            }
+        };
+        new Thread(r1).start();
+        new Thread(r2).start();
+    }
+
+}
